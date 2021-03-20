1bdeadc4e82e03aa1aca95800eb0242b8913cf9b
64ndr11x <80999752+64ndr11x@users.noreply.github.com>
2021-03-20 13:36:21 -0400

    Add files via upload

---
+++ acceso.js
@@ -0,0 +1,6 @@
+//Alerta de aceso
+var nombre;
+nombre=prompt("Escribe Tu Nombre Para Ingresar");
+alert("Muchas Gracias Bienvenido(a)" + " " + nombre);
+confirm("Deseas Entrar Al Sitio" + " " + nombre);
+document.write("Ta Has Registrado En XomeX Como:" + " " + nombre);
